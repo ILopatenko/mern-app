884b1154942ee5e9e48fa3595c72b2578e4bbcaa
Iurii Lopatenko <yalopatenko@gmail.com>
2022-03-24 22:44:36 -0700

    Base routes, async/await syntax. Ready to start working with DB
    Part 1. TC 00:36:30

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,40 @@
+const asyncHandler = require('express-async-handler');
+//@description: CREATE a new GOAL
+//      @route: POST at /api/goals
+//     @access: Private (the feature is in development)
+const createNewGoal = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Add required information!');
+  }
+  res.status(200).json({ message: `The new GOAL is created successfuly!` });
+};
+//@description: READ all the GOALS
+//      @route: GET at /api/goals
+//     @access: Private (the feature is in development)
+const readAllTheGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'All the GOALS are red successfuly!' });
+});
+//@description: UPDATE the GOAL by ID
+//      @route: PUT at /api/goals
+//     @access: Private (the feature is in development)
+const updateGoalByID = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `The GOAL with (id = ${req.params.id}) is updated successfuly!`,
+  });
+});
+//@description: DELETE the GOAL by ID
+//      @route: DELETE at /api/goals
+//     @access: Private (the feature is in development)
+const deleteGoalByID = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: `The GOAL (with id = ${req.params.id}) is deleted successfuly!`,
+  });
+});
+
+module.exports = {
+  createNewGoal,
+  readAllTheGoals,
+  updateGoalByID,
+  deleteGoalByID,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log(`hello`);
+//IMPORT block
+const express = require('express');
+const dotenv = require('dotenv/config');
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+//Setup variables
+const PORT = process.env.PORT || 5555;
+
+//Create a new instance of express server
+const app = express();
+
+//Add body parser and json middlewares to the server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Setup server routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+//Setup error handler
+app.use(errorHandler);
+
+//Start the server on PORT
+app.listen(PORT, () => {
+  console.log(`Server is started on port ${PORT}`);
+});

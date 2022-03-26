7b87f6d880dbed5d0ea4b609c04572bf3693c054
Iurii Lopatenko <yalopatenko@gmail.com>
2022-03-26 13:28:49 -0700

    Login functionality. Part 3 is finished

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/register';
+const API_URL = '/api/users/';
 
 //REGISTER a new USER
 const register = async (userData) => {
-  const responce = await axios.post(API_URL, userData);
+  const responce = await axios.post(API_URL + 'register', userData);
+  console.log(responce.data);
+  if (responce.data) {
+    localStorage.setItem('user', JSON.stringify(responce.data.user));
+  }
+  return responce.data;
+};
+
+//LOGIN
+const login = async (userData) => {
+  const responce = await axios.post(API_URL + 'login', userData);
   console.log(responce.data);
   if (responce.data) {
     localStorage.setItem('user', JSON.stringify(responce.data.user));
@@ -20,6 +30,7 @@ const logout = () => {
 const authService = {
   register,
   logout,
+  login,
 };
 
 export default authService;
